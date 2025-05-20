781d0fa8eda464ba94e8687b10a3c0f5ea2fce68
Anton Todorov <antontodorov321@gmail.com>
2025-05-20 20:11:08 +0300

    Implement variations tasks

---
+++ Combinatorial Problems/Variations with Repetition/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+string[] input = Console.ReadLine().Split(" ");
+int k = int.Parse(Console.ReadLine());
+string[] result = new string[k];
+
+VariationsWithRepetition(0);
+
+void VariationsWithRepetition(int index)
+{
+    if (index == result.Length)
+    {
+        Console.WriteLine(string.Join(" ", result));
+        return;
+    }
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        result[index] = input[i];
+        VariationsWithRepetition(index + 1);
+    }
+}
---
+++ Combinatorial Problems/Variations without Repetitions/Program.cs
@@ -0,0 +1,27 @@
+﻿
+string[] input = Console.ReadLine().Split(" ");
+int k = int.Parse(Console.ReadLine());
+string[] result = new string[k];
+bool[] used = new bool[input.Length];
+
+Variations(0);
+
+void Variations(int index)
+{
+    if (index == result.Length)
+    {
+        Console.WriteLine(string.Join(" ", result));
+        return;
+    }
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (!used[i])
+        {
+            result[index] = input[i];
+            used[i] = true;
+            Variations(index + 1);
+            used[i] = false;
+        }
+    }
+}
