21ddd87d23bf914e896af7e6ef1a682fd91224f7
Anton Todorov <antontodorov321@gmail.com>
2025-05-20 21:46:37 +0300

    Implement combinations tasks

---
+++ Combinatorial Problems/Combinations with Repetition/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string[] input = Console.ReadLine().Split();
+int k = int.Parse(Console.ReadLine());
+string[] result = new string[k];
+bool[] used = new bool[input.Length];
+
+Combinations(0, 0);
+
+void Combinations(int index, int startIndex)
+{
+    if (index == result.Length)
+    {
+        Console.WriteLine(string.Join(" ", result));
+        return;
+    }
+
+    for (int i = startIndex; i < input.Length; i++)
+    {
+        result[index] = input[i];
+        Combinations(index + 1, i);
+
+    }
+}
---
+++ Combinatorial Problems/Combinations without Repetition/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string[] input = Console.ReadLine().Split();
+int k = int.Parse(Console.ReadLine());
+string[] result = new string[k];
+bool[] used = new bool[input.Length];
+
+Combinations(0, 0);
+
+void Combinations(int index, int startIndex)
+{
+    if (index == result.Length)
+    {
+        Console.WriteLine(string.Join(" ", result));
+        return;
+    }
+
+    for (int i = startIndex; i < input.Length; i++)
+    {
+        if (!used[i])
+        {
+            result[index] = input[i];
+            used[i] = true;
+
+            Combinations(index + 1, i + 1);
+
+            used[i] = false;
+        }
+    }
+}
