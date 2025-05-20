62145296428ad1501952be189229c65be2395c3a
Anton Todorov <antontodorov321@gmail.com>
2025-05-20 22:04:36 +0300

    Implement Pascal's triangle

---
+++ Combinatorial Problems/N Choose K Count/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+int k = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetBinom(n, k));
+
+int GetBinom(int row, int col)
+{
+    if (row <= 1 || col == 0 || col == row)
+    {
+        return 1;
+    }
+
+    return GetBinom(row - 1, col) + GetBinom(row - 1, col - 1);
+}
