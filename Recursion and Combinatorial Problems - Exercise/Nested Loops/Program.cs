4dea6deceb20200015b022de09c62645ab26f8bf
Anton Todorov <antontodorov321@gmail.com>
2025-05-24 14:08:22 +0300

    Implement nested loops permutation

---
+++ Recursion and Combinatorial Problems - Exercise/Nested Loops/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+int[] output = new int[n];
+
+Permute(0);
+
+void Permute(int index)
+{
+    if (index == n)
+    {
+        Console.WriteLine(string.Join(" ", output));
+        return;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        output[index] = i;
+        Permute(index + 1);
+    }
+}
