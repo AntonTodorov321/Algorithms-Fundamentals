ea0f8bbaf8befa76eb44310b912adb643d9a8452
Anton Todorov <antontodorov321@gmail.com>
2025-05-24 13:54:23 +0300

    Implement reverse array recursively

---
+++ Recursion and Combinatorial Problems - Exercise/Reverse Array/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+ReverseNumbers(0);
+
+void ReverseNumbers(int index)
+{
+    if (index >= input.Length / 2)
+    {
+        Console.WriteLine(string.Join(" ", input));
+        return;
+    }
+
+    Swap(index, input.Length - index - 1);
+    ReverseNumbers(index + 1);
+}
+
+void Swap(int first, int second)
+{
+    int temp = input[first];
+    input[first] = input[second];
+    input[second] = temp;
+}
