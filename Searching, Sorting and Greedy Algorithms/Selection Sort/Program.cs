1940c9b3fc013aead7244efbfe8374edec5ca73b
Anton Todorov <antontodorov321@gmail.com>
2025-05-29 21:56:09 +0300

    Implement selection sort

---
+++ Searching, Sorting and Greedy Algorithms/Selection Sort/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+int[] elements = Console.ReadLine()
+                  .Split(" ")
+                  .Select(int.Parse)
+                  .ToArray();
+
+SelectionSort();
+
+Console.WriteLine(string.Join(" ", elements));
+
+void SelectionSort()
+{
+    for (int i = 0; i < elements.Length; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < elements.Length; j++)
+        {
+            if (elements[j] < elements[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        Swap(i, minIndex);
+    }
+}
+
+void Swap(int first, int second)
+{
+    int temp = elements[first];
+    elements[first] = elements[second];
+    elements[second] = temp;
+}
