41f2866fffba8d8348c145d0b40643e071f951dd
Anton Todorov <antontodorov321@gmail.com>
2025-05-29 21:30:31 +0300

    Implement binary search

---
+++ Searching, Sorting and Greedy Algorithms/Binary Search/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int[] elements = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+int searchingElement = int.Parse(Console.ReadLine());
+
+Console.WriteLine(BinarySearch());
+
+int BinarySearch()
+{
+    int start = 0;
+    int end = elements.Length - 1;
+
+    while (start <= end)
+    {
+        int middle = (start + end) / 2;
+
+        if (elements[middle] > searchingElement)
+        {
+            end = middle - 1;
+        }
+        else if (elements[middle] < searchingElement)
+        {
+            start = middle + 1;
+        }
+        else
+        {
+            return middle;
+        }
+    }
+
+    return -1;
+}
