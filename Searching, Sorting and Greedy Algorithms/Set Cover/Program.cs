57c6edafa59d1489d1c4fcad23ff6cfd9f0005a7
Anton Todorov <antontodorov321@gmail.com>
2025-06-01 11:56:16 +0300

    Implement set cover task

---
+++ Searching, Sorting and Greedy Algorithms/Set Cover/Program.cs
@@ -0,0 +1,46 @@
+﻿
+HashSet<int> universe = Console.ReadLine()!
+                        .Split(", ")
+                        .Select(int.Parse)
+                        .ToHashSet();
+
+int numberOfSets = int.Parse(Console.ReadLine()!);  
+List<int[]> sets = new List<int[]>();
+List<int[]> result = new List<int[]>();
+
+for (int i = 0; i < numberOfSets; i++)
+{
+    int[] set = Console.ReadLine()!
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+    sets.Add(set);
+}
+
+Universe();
+
+Console.WriteLine($"Sets to take ({result.Count}):");
+
+foreach (var set in result)
+{
+    Console.WriteLine(string.Join(", ", set));
+}
+
+void Universe()
+{
+    while (universe.Count > 0)
+    {
+        int[] set = sets
+            .OrderByDescending(set => set.Count(s => universe.Contains(s)))
+            .FirstOrDefault()!;
+
+        result.Add(set);
+        sets.Remove(set);
+
+        foreach (var number in set)
+        {
+            universe.Remove(number);
+        }
+    }
+}
