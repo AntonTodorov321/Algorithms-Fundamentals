3889cd60ea6b450275a58ca2deffd3d662cf108c
Anton Todorov <antontodorov321@gmail.com>
2025-06-13 22:47:29 +0300

    Implement connected components in graph

---
+++ Lab Graph Theory, Traversal, and Shortest Paths/Connected Components/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Runtime.CompilerServices;
+
+int n = int.Parse(Console.ReadLine());
+List<int>[] graph = new List<int>[n];
+bool[] visited = new bool[n];
+
+for (int node = 0; node < n; node++)
+{
+    string input = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(input))
+    {
+        graph[node] = new List<int>();
+        continue;
+    }
+
+    List<int> children = input
+                   .Split(" ")
+                   .Select(int.Parse)
+                   .ToList();
+
+    graph[node] = children;
+}
+
+for (int node = 0; node < n; node++)
+{
+    if (!visited[node])
+    {
+        List<int> component = new List<int>();
+        DFS(node, component);
+
+        Console.WriteLine($"Connected component: {string.Join(" ", component)}");
+    }
+}
+
+void DFS(int node, List<int> component)
+{
+    if (visited[node])
+    {
+        return;
+    }
+
+    visited[node] = true;
+
+    foreach (var child in graph[node])
+    {
+        DFS(child, component);
+    }
+
+    component.Add(node);
+}
