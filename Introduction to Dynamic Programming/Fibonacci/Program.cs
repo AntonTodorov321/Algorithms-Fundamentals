0471999c47d58f25246347c17eb245759f77d2d8
Anton Todorov <antontodorov321@gmail.com>
2025-07-12 11:35:13 +0300

    Implement fibonacci with memoization

---
+++ Introduction to Dynamic Programming/Fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Fibonacci
+{
+    using System;
+
+    internal class Program
+    {
+        private static Dictionary<int, long> memo = new Dictionary<int, long>();
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            long fibonacci = Fibonacci(n);
+
+            Console.WriteLine(fibonacci);
+        }
+
+        private static long Fibonacci(int n)
+        {
+            if (memo.ContainsKey(n))
+            {
+                return memo[n];
+            }
+
+            if (n < 2)
+            {
+                return n;
+            }
+
+            long result = Fibonacci(n - 1) + Fibonacci(n - 2);
+            memo[n] = result;
+
+            return result;
+        }
+    }
+}
