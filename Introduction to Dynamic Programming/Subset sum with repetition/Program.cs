52b8ec0e35770a5f1e103d7f2a5ba5d8a1219e4c
Anton Todorov <antontodorov321@gmail.com>
2025-07-13 13:43:34 +0300

    Implement subset sum with dynamic programing

---
+++ Introduction to Dynamic Programming/Subset sum with repetition/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Subset_sum_with_repetition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [2, 7];
+            int target = 325;
+
+            bool[] sums = new bool[target + 1];
+            sums[0] = true;
+
+            for (int sum = 0; sum < sums.Length; sum++)
+            {
+                if (!sums[sum])
+                {
+                    continue;
+                }
+
+                foreach (var num in nums)
+                {
+                    int newSum = num + sum;
+
+                    if (newSum > target)
+                    {
+                        continue;
+                    }
+
+                    sums[newSum] = true;
+                }
+            }
+
+            List<int> subset = new List<int>();
+
+            while (target > 0)
+            {
+                foreach (var num in nums)
+                {
+                    int prevSum = target - num;
+
+                    if (prevSum >= 0 && sums[prevSum])
+                    {
+                        subset.Add(num);
+                        target = prevSum;
+
+                        if (target == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", subset));
+        }
+    }
+}
---
+++ Introduction to Dynamic Programming/Subset sum/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Subset_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 3, 5, 1, 4, 2 };
+            int target = 20;
+
+            Dictionary<int, int> possibleSums = GetAllPossibleSums(nums);
+
+            if (possibleSums.ContainsKey(target))
+            {
+                List<int> subset = FindSubset(possibleSums, target);
+                Console.WriteLine(string.Join(" ", subset));
+            }
+            else
+            {
+                Console.WriteLine("Can not make the given target with these numbers");
+            }
+
+        }
+
+        private static List<int> FindSubset(Dictionary<int, int> sums, int target)
+        {
+            List<int> subset = new List<int>();
+
+            while (target > 0)
+            {
+                int currentNum = sums[target];
+
+                subset.Add(currentNum);
+                target -= currentNum;
+            }
+
+            return subset;
+        }
+
+        private static Dictionary<int, int> GetAllPossibleSums(int[] nums)
+        {
+            Dictionary<int, int> sums = new Dictionary<int, int> { { 0, 0 } };
+
+            foreach (var num in nums)
+            {
+                int[] currentSums = sums.Keys.ToArray();
+
+                foreach (var sum in currentSums)
+                {
+                    int newSum = sum + num;
+
+                    if (!sums.ContainsKey(newSum))
+                    {
+                        sums.Add(sum + num, num);
+                    }
+                }
+            }
+
+            return sums;
+        }
+    }
+}
