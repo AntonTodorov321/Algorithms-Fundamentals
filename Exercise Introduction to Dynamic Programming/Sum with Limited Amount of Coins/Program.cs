5e9667a74e6b01523c75d6b183482a28b43d5232
Anton Todorov <antontodorov321@gmail.com>
2025-07-27 21:26:34 +0300

    Implement subset sum with dynamic programing

---
+++ Exercise Introduction to Dynamic Programming/Sum with Limited Amount of Coins/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Sum_with_Limited_Amount_of_Coins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
+
+            int target = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountSum(numbers, target));
+        }
+
+        private static int CountSum(int[] numbers, int target)
+        {
+            int count = 0;
+            HashSet<int> sums = new HashSet<int>() { 0 };
+
+            foreach (var number in numbers)
+            {
+                HashSet<int> currentSums = new HashSet<int>();
+
+                foreach (var sum in sums)
+                {
+                    int currentSum = sum + number;
+
+                    if (currentSum == target)
+                    {
+                        count++;
+                    }
+
+                    currentSums.Add(currentSum);
+                }
+
+                sums = sums.Union(currentSums).ToHashSet();
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Exercise Introduction to Dynamic Programming/Sum with Unlimited Amount of Coins/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Sum_with_Unlimited_Amount_of_Coins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
+
+            int target = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountSum(numbers, target));
+            ;
+        }
+
+        private static int CountSum(int[] numbers, int target)
+        {
+            int[] sums = new int[target + 1];
+            sums[0] = 1;
+
+            foreach (var number in numbers)
+            {
+                for (int sum = number; sum < sums.Length; sum++)
+                {
+                    sums[sum] += sums[sum - number];
+                }
+            }
+
+            return sums[target];
+        }
+    }
+}
